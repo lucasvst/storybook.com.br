d8bd16ed0c7f94762b75890971b01ccd1115dcfc
Lucas Teles <lucasvst@gmail.com>
2024-08-02 21:45:35 -0300

    adds google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import GoogleAdsense from "@/components/GoogleAdsense"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -16,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAdsense pId="3829371704703262" />
     </html>
   )
 }
